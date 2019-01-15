54d3b9a012cbfb6dc509f7b830e184d3b462dc7b
Andrew Ouatu <aouatu@outlook.com>
2019-01-15 15:09:51 -0800

    Completed jwt sign in on server side

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -5,17 +5,20 @@ import AccountRoutes from './account';
 import { Route, Switch } from 'react-router-dom';
 import NotFound from './general/404';
 import Nav from './nav';
+import Home from './home';
+
 
 const App = () => (
     <div>
         <Nav/>
         <div className="container">
             <Switch>
+                <Route exact path="/" component={Home}/>
                 <Route path="/account" component={AccountRoutes}/>
                 <Route component={NotFound}/>
             </Switch>
         </div>
     </div>
-);
+)
 
 export default App;
